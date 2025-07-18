625ce9285263eccc3f1d5c074a59648f09a45c60
aliirfan23 <aaliirfan69@gmail.com>
2025-07-18 16:49:40 +0500

    spring security with csrf enabled

--- session5/src/main/java/com/redmath/security/SecurityConfig.java
+++ session5/src/main/java/com/redmath/security/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.redmath.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Slf4j
 @Configuration
@@ -21,19 +25,27 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.info("Configuring security filter chain");
+        http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
         return http
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/h2-console/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/news/**").hasRole("REPORTER")
                         .requestMatchers(  "/api/v1/{newsId}").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .csrf(csrf->csrf.disable())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
-                .headers(headers -> headers.disable())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(PathRequest.toH2Console()) //Disable CSRF for H2 Console
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin) //Allow H2 in frames
+                )
+//                .headers(headers -> headers.disable())
                 .build();
+
     }
 
 }
